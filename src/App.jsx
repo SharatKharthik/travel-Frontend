202c07e73e831936859d52967e85f3bb37ad6660
SharatKharthik <163654735+SharatKharthik@users.noreply.github.com>
2025-10-18 10:54:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/travel">
       <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
